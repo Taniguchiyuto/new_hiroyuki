85f0addc6544e5e6144e677c70a346e5385246ab
Yuto Taniguchi <youdougukou5@gmail.com>
2024-09-06 20:51:50 +0900

    gpt-apiキーを使用したアプリ完成

--- lib/page_three.dart
+++ lib/page_three.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PageThree extends StatelessWidget {
@@ -73,12 +74,6 @@ class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
   final ScrollController _scrollController = ScrollController();
   List<Map<String, String>> messages = [];
-<<<<<<< HEAD
-  final String apiKey =
-      'xxxxxx'; // OpenAI APIキー
-=======
-
-  // OpenAI APIキー
   final String apiKey = dotenv.env['API_KEY'] ?? '';
 
   // GPT APIを呼び出す関数
