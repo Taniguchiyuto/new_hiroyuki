6a8ec39119b0ab9122b205a8f516716aadac51d0
Yuto Taniguchi <youdougukou5@gmail.com>
2024-09-06 21:51:48 +0900

    kkoko

--- lib/page_three.dart
+++ lib/page_three.dart
@@ -3,8 +3,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class PageThree extends StatelessWidget {
   final List<ChatItem> chatItems = [
     ChatItem("ひろゆき", "それってあなたの感想ですよね", "14:57", "assets/images/avatar1.png"),
